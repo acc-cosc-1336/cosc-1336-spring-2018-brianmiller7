8a8144756778950d680f1504b1a93244af04bc6e
brianmiller7 <35745887+brianmiller7@users.noreply.github.com>
2018-01-25 21:43:05 -0600

    removing :

--- src/assignments/main_assignment2.py
+++ src/assignments/main_assignment2.py
@@ -1,4 +1,4 @@
 from assignment2 import faculty_evaluation_result
 
 '''Write code to call the faculty_evaluation_result function with data of your choice'''
-faculty_evaluation_result(10,10,10,10,10,10):
+faculty_evaluation_result(10,10,10,10,10,10)
