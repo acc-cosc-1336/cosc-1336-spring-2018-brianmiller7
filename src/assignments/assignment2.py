c4a253be80d033a07e89efeae721b8a4a77fcccb
brianmiller7 <35745887+brianmiller7@users.noreply.github.com>
2018-01-25 21:26:36 -0600

    fixing typo with double ratio

--- src/assignments/assignment2.py
+++ src/assignments/assignment2.py
@@ -10,25 +10,28 @@ def faculty_evaluation_result(nev, rar, som, oft, voft, alw):
     :param alw: Always
     :return: rating as a string
     '''
+	# Calc total Responses
 	totalResp = nev + rar + som + oft + voft + alw
-	alw_ratio = alw // totalResp
-	voft_ratio = voft // totalResp
-	oft_ratio = oft // totalResp
-	som_ratio = som // totalResp
-	rar_ratio = rar // totalResp
-	nev_ratio = nev // totalResp
+	
+	# Calc individual ratios
+	alw_ratio = alw / totalResp
+	voft_ratio = voft / totalResp
+	oft_ratio = oft / totalResp
+	som_ratio = som / totalResp
+	rar_ratio = rar / totalResp
+	nev_ratio = nev / totalResp
 	
 	# Test for Excellent
-	if (alw_ratio + voft_ratio_ratio) >= .9 :
+	if (alw_ratio + voft_ratio) >= .9 :
 		return 'Excellent'
 	# Test for Very Good
-	elif (oft_ratio + voft_ratio_ratio + alw_ratio) >= .8 :
+	elif (oft_ratio + voft_ratio + alw_ratio) >= .8 :
 		return 'Very Good'
 	# Test for Good
-	elif (som_ratio + oft_ratio + voft_ratio_ratio + alw_ratio) >= .7 :
+	elif (som_ratio + oft_ratio + voft_ratio + alw_ratio) >= .7 :
 		return 'Good'
 	# Test for NI
-	elif (rar_ratio + som_ratio + oft_ratio + voft_ratio_ratio + alw_ratio) >= .6 :
+	elif (rar_ratio + som_ratio + oft_ratio + voft_ratio + alw_ratio) >= .6 :
 		return 'NI'
 	# Test for Unacceptable
 	else :
@@ -49,6 +52,4 @@ def get_ratings(nev,rar,som, oft,voft, alw):
     ratings.append(round(nev / total, 2))
 
     return ratings
-    
-
     
