d8838666363042e744d314eb60bad84741aa12ad
brianmiller7 <35745887+brianmiller7@users.noreply.github.com>
2018-01-25 21:40:48 -0600

    Removing comments, checked in IDLE

--- src/assignments/assignment2.py
+++ src/assignments/assignment2.py
@@ -10,32 +10,25 @@ def faculty_evaluation_result(nev, rar, som, oft, voft, alw):
     :param alw: Always
     :return: rating as a string
     '''
-# Calc total Responses
-totalResp = nev + rar + som + oft + voft + alw
+    totalResp = nev + rar + som + oft + voft + alw
 	
-# Calc individual ratios
-alw_ratio = alw / totalResp
-voft_ratio = voft / totalResp
-oft_ratio = oft / totalResp
-som_ratio = som / totalResp
-rar_ratio = rar / totalResp
-nev_ratio = nev / totalResp
+    alw_ratio = alw / totalResp
+    voft_ratio = voft / totalResp
+    oft_ratio = oft / totalResp
+    som_ratio = som / totalResp
+    rar_ratio = rar / totalResp
+    nev_ratio = nev / totalResp
 	
-# Test for Excellent
-if (alw_ratio + voft_ratio) >= .9 :
-	return 'Excellent'
-# Test for Very Good
-elif (oft_ratio + voft_ratio + alw_ratio) >= .8 :
-	return 'Very Good'
-# Test for Good
-elif (som_ratio + oft_ratio + voft_ratio + alw_ratio) >= .7 :
-	return 'Good'
-# Test for NI
-elif (rar_ratio + som_ratio + oft_ratio + voft_ratio + alw_ratio) >= .6 :
-	return 'Needs Improvement'
-# Test for Unacceptable
-else :
-	return 'Unacceptable'
+    if (alw_ratio + voft_ratio_ratio) >= .9 :
+	    return 'Excellent'
+    elif (oft_ratio + voft_ratio_ratio + alw_ratio) >= .8 :
+	    return 'Very Good'
+    elif (som_ratio + oft_ratio + voft_ratio_ratio + alw_ratio) >= .7 :
+	    return 'Good'
+    elif (rar_ratio + som_ratio + oft_ratio + voft_ratio_ratio + alw_ratio) >= .6 :
+	    return 'Needs Improvement'
+    else :
+	    return 'Unacceptable'
 
 def get_ratings(nev,rar,som, oft,voft, alw):
     '''
@@ -52,4 +45,3 @@ def get_ratings(nev,rar,som, oft,voft, alw):
     ratings.append(round(nev / total, 2))
 
     return ratings
-    
